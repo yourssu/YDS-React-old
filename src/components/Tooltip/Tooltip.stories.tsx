00b0a322eaa89ba141d575a8001affcc1a5076cb
Hyomin Koo <me@eatsteak.dev>
2022-04-05 22:48:03 +0900

    feat: implement tooltip base

---
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Tooltip } from './Tooltip';
+
+export default {
+  title: 'Component/Tooltip',
+  component: Tooltip,
+  argTypes: {},
+} as ComponentMeta<typeof Tooltip>;
+
+const Template: ComponentStory<typeof Tooltip> = (args) => (<div style={{ backgroundColor: '#c4c4c4eb', width: '374px', height: '400px', paddingTop: '16px', paddingBottom: '16px' }}><Tooltip {...args} /></div>);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  arrowPosition: 'top',
+  arrowAlign: 'start',
+  children: '홈에서 실시간 피드를 확인해보세요!',
+};
