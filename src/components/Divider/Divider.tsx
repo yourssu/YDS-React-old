9af573701e38329303141284d707dabb0f565a94
jaewoong2 <jwisgenius@naver.com>
2022-04-06 00:32:03 +0900

    add/ EATSTEAKs

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Theme } from '@emotion/react'
+
+export interface DividerProps {
+  thickness: 'thin' | 'thick'
+  direction: 'horizontal' | 'vertical'
+}
+
+const thicknessDef = {
+  thin: '0.1px',
+  thick: '8px',
+}
+
+const colorDef = (thickness: 'thin' | 'thick', theme: Theme): string =>
+  ({
+    thin: theme.color.borderNormal,
+    thick: theme.color.borderThin,
+  }[thickness])
+
+export const Divider = styled.hr<DividerProps>`
+  width: ${({ direction }) => (direction === 'horizontal' ? '100%' : 'unset')};
+  height: ${({ direction }) => (direction === 'vertical' ? '100%' : 'unset')};
+  border-width: ${({ direction, thickness }) =>
+    direction === 'horizontal' ? `0 0 ${thicknessDef[thickness]} 0` : `0 0 0 ${thicknessDef[thickness]}`};
+  border-color: ${({ thickness, theme }) => colorDef(thickness, theme)};
+`
