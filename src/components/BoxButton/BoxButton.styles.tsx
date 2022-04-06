452160927d00026dcd84a4c65845223882115a82
jaewoong2 <jwisgenius@naver.com>
2022-04-06 15:20:10 +0900

    refactoring/ Box Button

--- src/components/BoxButton/BoxButton.styles.tsx
+++ src/components/BoxButton/BoxButton.styles.tsx
@@ -3,15 +3,19 @@ import styled from '@emotion/styled'
 import { getTypoStyle, Typography } from '../../styles/foundation/typo/typo'
 import { BoxButtonProps } from '../../types'
 
-type getStyleType = { types: BoxButtonProps['types']; theme: Theme }
+type getStyleType = {
+  types: BoxButtonProps['types']
+  theme?: Theme
+  size?: BoxButtonProps['size']
+}
 
 const getDisabledStyle = ({ types, theme }: getStyleType) => {
   if (types === 'filled') {
     return css`
-      background-color: ${theme.color.buttonDisabledBG};
-      color: ${theme.color.buttonDisabled};
+      background-color: ${theme?.color.buttonDisabledBG};
+      color: ${theme?.color.buttonDisabled};
       border: none;
-      fill: ${theme.color.buttonDisabled};
+      fill: ${theme?.color.buttonDisabled};
       &:hover {
         cursor: not-allowed;
       }
@@ -20,10 +24,10 @@ const getDisabledStyle = ({ types, theme }: getStyleType) => {
 
   if (types === 'line') {
     return css`
-      background-color: ${theme.color.bgNormal};
-      border: 0.1px solid ${theme.color.buttonDisabled};
-      color: ${theme.color.buttonDisabled};
-      fill: ${theme.color.buttonDisabled};
+      background-color: ${theme?.color.bgNormal};
+      border: 0.1px solid ${theme?.color.buttonDisabled};
+      color: ${theme?.color.buttonDisabled};
+      fill: ${theme?.color.buttonDisabled};
       &:hover {
         cursor: not-allowed;
       }
@@ -32,10 +36,10 @@ const getDisabledStyle = ({ types, theme }: getStyleType) => {
 
   if (types === 'tinted') {
     return css`
-      background-color: ${theme.color.buttonDisabledBG};
-      color: ${theme.color.buttonDisabled};
+      background-color: ${theme?.color.buttonDisabledBG};
+      color: ${theme?.color.buttonDisabled};
       border: none;
-      fill: ${theme.color.buttonDisabled};
+      fill: ${theme?.color.buttonDisabled};
       &:hover {
         cursor: not-allowed;
       }
@@ -46,40 +50,40 @@ const getDisabledStyle = ({ types, theme }: getStyleType) => {
 const getNormalStyle = ({ types, theme }: getStyleType) => {
   if (types === 'filled') {
     return css`
-      background-color: ${theme.color.buttonPoint};
-      color: ${theme.color.buttonReserved};
-      fill: ${theme.color.buttonReserved};
+      background-color: ${theme?.color.buttonPoint};
+      color: ${theme?.color.buttonReserved};
+      fill: ${theme?.color.buttonReserved};
       border: none;
 
       &:hover {
-        background-color: ${theme.color.buttonPointPressed};
+        background-color: ${theme?.color.buttonPointPressed};
       }
     `
   }
 
   if (types === 'line') {
     return css`
-      background-color: ${theme.color.bgNormal};
-      color: ${theme.color.buttonPoint};
-      border: 0.1px solid ${theme.color.buttonPoint};
-      fill: ${theme.color.buttonPoint};
+      background-color: ${theme?.color.bgNormal};
+      color: ${theme?.color.buttonPoint};
+      border: 0.1px solid ${theme?.color.buttonPoint};
+      fill: ${theme?.color.buttonPoint};
       &:hover {
-        border: 0.1px solid ${theme.color.buttonPointPressed};
-        color: ${theme.color.buttonPointPressed};
-        fill: ${theme.color.buttonPointPressed};
+        border: 0.1px solid ${theme?.color.buttonPointPressed};
+        color: ${theme?.color.buttonPointPressed};
+        fill: ${theme?.color.buttonPointPressed};
       }
     `
   }
 
   if (types === 'tinted') {
     return css`
-      background-color: ${theme.color.buttonPointBG};
-      color: ${theme.color.buttonPoint};
+      background-color: ${theme?.color.buttonPointBG};
+      color: ${theme?.color.buttonPoint};
       border: none;
-      fill: ${theme.color.buttonPoint};
+      fill: ${theme?.color.buttonPoint};
       &:hover {
-        color: ${theme.color.buttonPointPressed};
-        fill: ${theme.color.buttonPointPressed};
+        color: ${theme?.color.buttonPointPressed};
+        fill: ${theme?.color.buttonPointPressed};
       }
     `
   }
@@ -88,45 +92,91 @@ const getNormalStyle = ({ types, theme }: getStyleType) => {
 const getWarnedStyle = ({ types, theme }: getStyleType) => {
   if (types === 'filled') {
     return css`
-      background-color: ${theme.color.buttonWarned};
-      color: ${theme.color.buttonReserved};
+      background-color: ${theme?.color.buttonWarned};
+      color: ${theme?.color.buttonReserved};
       border: none;
-      fill: ${theme.color.buttonReserved};
+      fill: ${theme?.color.buttonReserved};
       &:hover {
         cursor: pointer;
-        background-color: ${theme.color.buttonWarnedPressed};
+        background-color: ${theme?.color.buttonWarnedPressed};
       }
     `
   }
 
   if (types === 'line') {
     return css`
-      background-color: ${theme.color.bgNormal};
-      color: ${theme.color.buttonWarned};
-      border: 0.1px solid ${theme.color.buttonWarned};
-      fill: ${theme.color.buttonWarned};
+      background-color: ${theme?.color.bgNormal};
+      color: ${theme?.color.buttonWarned};
+      border: 0.1px solid ${theme?.color.buttonWarned};
+      fill: ${theme?.color.buttonWarned};
       &:hover {
         cursor: pointer;
-        fill: ${theme.color.buttonWarnedPressed};
-        color: ${theme.color.buttonWarnedPressed};
-        border-color: ${theme.color.buttonWarnedPressed};
+        fill: ${theme?.color.buttonWarnedPressed};
+        color: ${theme?.color.buttonWarnedPressed};
+        border-color: ${theme?.color.buttonWarnedPressed};
       }
     `
   }
 
   if (types === 'tinted') {
     return css`
-      background-color: ${theme.color.buttonWarnedBG};
-      color: ${theme.color.buttonWarned};
+      background-color: ${theme?.color.buttonWarnedBG};
+      color: ${theme?.color.buttonWarned};
       border: none;
-      fill: ${theme.color.buttonWarned};
+      fill: ${theme?.color.buttonWarned};
 
       &:hover {
         cursor: pointer;
-        fill: ${theme.color.buttonWarnedPressed};
-        color: ${theme.color.buttonWarnedPressed};
+        fill: ${theme?.color.buttonWarnedPressed};
+        color: ${theme?.color.buttonWarnedPressed};
       }
     `
+  }
+}
+
+const getSizeStyle = ({ size, types }: getStyleType) => {
+  if (size === 'extraLarge') {
+    if (types === 'filled') {
+      return css`
+        height: 56px;
+        font-size: 16px;
+      `
+    } else {
+      return css`
+        display: none;
+      `
+    }
+  }
+
+  if (size === 'large') {
+    return css`
+      height: 48px;
+      font-size: 14px;
+    `
+  }
+
+  if (size === 'medium') {
+    css`
+      height: 40px;
+      font-size: 14px;
+    `
+  }
+
+  if (size === 'small') {
+    if (types === 'filled') {
+      css`
+        display: none;
+      `
+    } else {
+      css`
+        height: 32px;
+        font-size: 12px;
+        svg {
+          width: 16px;
+          height: 16px;
+        }
+      `
+    }
   }
 }
 
@@ -137,46 +187,11 @@ export const Button = styled.button<BoxButtonProps>`
   justify-content: center;
   padding: 0 16px 0 16px;
   border-radius: ${({ rounding }) => rounding}px;
-  &:hover {
-    cursor: pointer;
-  }
+  cursor: pointer;
   ${({ types, theme }) => getNormalStyle({ types, theme })};
   ${({ isWarned, theme, types }) => isWarned && getWarnedStyle({ theme, types })};
   &:disabled {
     ${({ types, theme }) => getDisabledStyle({ types, theme })};
   }
-
-  ${({ size, types }) =>
-    size === 'extraLarge'
-      ? types === 'filled'
-        ? css`
-            height: 56px;
-            font-size: 16px;
-          `
-        : css`
-            display: none;
-          `
-      : size === 'large'
-      ? css`
-          height: 48px;
-          font-size: 14px;
-        `
-      : size === 'medium'
-      ? css`
-          height: 40px;
-          font-size: 14px;
-        `
-      : // size === 'small'
-      types === 'filled'
-      ? css`
-          display: none;
-        `
-      : css`
-          height: 32px;
-          font-size: 12px;
-          svg {
-            width: 16px;
-            height: 16px;
-          }
-        `};
+  ${({ size, types }) => getSizeStyle({ size, types })};
 `
