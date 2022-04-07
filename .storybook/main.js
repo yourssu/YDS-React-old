3384b86ebb9e26126cd8f430bd407a6197044ed7
jaewoong2 <jwisgenius@naver.com>
2022-04-07 16:55:05 +0900

    fix/ confilct

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ const resolvePath = (_path) => path.join(process.cwd(), _path)
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
-  staticDirs: ['../static'],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
