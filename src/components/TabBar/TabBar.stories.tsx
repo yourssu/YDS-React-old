f6a3b97098aee474d58a2b1612b668d5ece99d9f
EATSTEAK <me@eatsteak.dev>
2022-05-05 19:56:21 +0900

    feat: add div to template for easier scroll test

--- src/components/TabBar/TabBar.stories.tsx
+++ src/components/TabBar/TabBar.stories.tsx
@@ -1,44 +1,55 @@
-import React from 'react'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
-import { TabBar } from './TabBar'
-import { Tab } from './Tab'
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { TabBar } from "./TabBar";
+import { Tab } from "./Tab";
 
 export default {
-  title: 'Component/TabBar',
+  title: "Component/TabBar",
   component: TabBar,
-  argTypes: {},
-} as ComponentMeta<typeof TabBar>
+  argTypes: {}
+} as ComponentMeta<typeof TabBar>;
 
 /** !! */
 const Template: ComponentStory<typeof TabBar> = (args) => (
-  <TabBar scrollable={args.scrollable} navigation={args.navigation} onChange={args.onChange}>
-    <Tab value="1" selected>
-      Tab 1
-    </Tab>
-    <Tab value="2">Tab 2</Tab>
-    <Tab value="3">Tab 3</Tab>
-    <Tab value="4">Tab 4</Tab>
-    <Tab value="5">Tab 5</Tab>
-    <Tab value="6">Tab 6</Tab>
-    <Tab value="7">Tab 7</Tab>
-    <Tab value="8">Tab 8</Tab>
-  </TabBar>
-)
+  <div
+    style={{
+      backgroundColor: "#c4c4c4eb",
+      width: "374px",
+      height: "200px",
+      paddingTop: "16px",
+      paddingBottom: "16px"
+    }}
+  >
+    <TabBar scrollable={args.scrollable} navigation={args.navigation}
+            onChange={args.onChange}>
+      <Tab value="1" selected>
+        Tab 1
+      </Tab>
+      <Tab value="2">Tab 2</Tab>
+      <Tab value="3">Tab 3</Tab>
+      <Tab value="4">Tab 4</Tab>
+      <Tab value="5">Tab 5</Tab>
+      <Tab value="6">Tab 6</Tab>
+      <Tab value="7">Tab 7</Tab>
+      <Tab value="8">Tab 8</Tab>
+    </TabBar>
+  </div>
+);
 
-export const Scrollable = Template.bind({})
+export const Scrollable = Template.bind({});
 Scrollable.args = {
   scrollable: true,
-  navigation: false,
-}
+  navigation: false
+};
 
-export const ScrollableNavigation = Template.bind({})
+export const ScrollableNavigation = Template.bind({});
 ScrollableNavigation.args = {
   scrollable: true,
-  navigation: true,
-}
+  navigation: true
+};
 
-export const Fixed = Template.bind({})
+export const Fixed = Template.bind({});
 Fixed.args = {
   scrollable: false,
-  navigation: false,
-}
+  navigation: false
+};
