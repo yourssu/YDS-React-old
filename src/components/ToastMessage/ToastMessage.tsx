894bc29d889a6bddf8e6044c81b9b0a30684b283
jaewoong2 <jwisgenius@naver.com>
2022-06-19 18:47:55 +0900

    fix ToastMessage style

--- src/components/ToastMessage/ToastMessage.tsx
+++ src/components/ToastMessage/ToastMessage.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { ToastMessageProps } from '../../types'
 import * as Styled from './ToastMessage.styles'
 import { Toast } from './ToastMessage.styles'
 
 /* children: message of ToastMessage */
-const ToastMessage: React.FC<ToastMessageProps> = ({ children }) => (
-  <Styled.ToastWrapper>
-    <div className="toast-container">
-      <div className="padding--8" />
-      <Toast>
-        <div>{children}</div>
-      </Toast>
-      <div className="padding--8" />
-    </div>
-  </Styled.ToastWrapper>
-)
+
+const ToastMessage = forwardRef<HTMLDivElement, ToastMessageProps>
+(({ children }, ref) => {
+  return (
+    <Styled.ToastWrapper ref={ref}>
+      <div className="toast-container">
+        <div className="padding--8" />
+        <Toast>
+          <div>{children}</div>
+        </Toast>
+        <div className="padding--8" />
+      </div>
+    </Styled.ToastWrapper>
+  )
+})
+
 
 export { ToastMessage }
